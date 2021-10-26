e967509903c68b2bd23ce6b1ea280b901faeb47f
MarkusAllikas <markusallikas03@gmail.com>
2021-10-26 22:59:41 +0300

    2

--- Laskuharjoitus1/Laskuharjoitus1/Program.cs
+++ Laskuharjoitus1/Laskuharjoitus1/Program.cs
@@ -6,7 +6,11 @@ namespace Laskuharjoitus1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Anna x:lle arvo");
+            string x = Console.ReadLine();
+            Console.WriteLine("Anna y:lle arvo");
+            string y = Console.ReadLine();
+            Console.WriteLine (x = y + 3);
         }
     }
 }
