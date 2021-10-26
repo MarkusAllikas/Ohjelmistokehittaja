cb144fda3e0155d53dbfab3c53c47993e21f232e
MarkusAllikas <markusallikas03@gmail.com>
2021-10-26 13:02:10 +0300

    11

---
+++ Numero/Numero/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1");
+            Console.WriteLine("11");
+            Console.WriteLine("101");
+            Console.WriteLine("1001");
+        }
+    }
+}
---
+++ kayttajaNimi/kayttajaNimi/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace kayttajaNimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hei, kuka olet?");
+            string nimi = Console.ReadLine();
+            Console.WriteLine("Hei " + nimi + ", Tervetuloa!");
+        }
+    }
+}
