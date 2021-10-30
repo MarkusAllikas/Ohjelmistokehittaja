c10983246d87b9bf6baae5497b0a4c86450c9ce7
MarkusAllikas <markusallikas03@gmail.com>
2021-10-30 18:09:05 +0300

    keskEn, ifelse eitoimi

---
+++ kumpiSuurempi/kumpiSuurempi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace kumpiSuurempi
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anna yksi luku:");
+            int nro1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen luku:");
+            int nro2 = int.Parse(Console.ReadLine());
+            
+            //Kesken, ifelse ei toimi.
+            /*if(nro1 <= nro2)
+            {
+                Console.Write(nro1, nro2);
+            } else
+            {
+                Console.Write(nro2, nro1);
+            }*/
+        }
+    }
+}
