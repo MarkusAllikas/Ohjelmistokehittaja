b5b04500e7745cbd3d305703fe41576ec364cca5
Markus Allikas <markusallikas03@gmail.com>
2021-11-04 15:40:09 +0200

    1 11 101 1001

--- YksiNumerot/YksiNumerot/Program.cs
+++ YksiNumerot/YksiNumerot/Program.cs
@@ -9,7 +9,7 @@ namespace YksiNumerot
             Console.WriteLine("1");
             Console.WriteLine("11");
             Console.WriteLine("101");
-            Console.WriteLine("1001");
+            Console.WriteLine("1001"); //.
         }
     }
 }
