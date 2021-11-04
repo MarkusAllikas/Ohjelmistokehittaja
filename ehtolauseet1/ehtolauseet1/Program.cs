5a66a7bf08d3e726640496f71de12e4745d803c8
Markus Allikas <markusallikas03@gmail.com>
2021-11-04 14:05:51 +0200

    5/7 tehty

--- ehtolauseet1/ehtolauseet1/Program.cs
+++ ehtolauseet1/ehtolauseet1/Program.cs
@@ -7,7 +7,7 @@ namespace ehtolauseet1
         static void Main(string[] args)
         {
             // Tehtävä 1: Ehtolause, joka vertaa kahta annettua lukua ja tulostaa ne suuruusjärjestyksessä
-            Console.WriteLine("Tehtävä 1");
+            Console.WriteLine("Tehtävä 1: ");
             Console.WriteLine("Anna yksi luku");
             int x1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Anna toinen luku");
@@ -22,7 +22,7 @@ namespace ehtolauseet1
 
 
             // Tehtävä 2: Kysyy 3 lukua ja tulostaa niistä isoimman.
-            Console.WriteLine("Tehtävä 2");
+            Console.WriteLine("Tehtävä 2: ");
             Console.WriteLine("Anna yksi luku");
             int x2 = int.Parse(Console.ReadLine());
             int isoin = x2;
@@ -42,7 +42,7 @@ namespace ehtolauseet1
 
 
             // Tehtävä 3: Kysyy luvun 0-9 ja riippuen luvusta tulostaa sen sanana.
-            Console.WriteLine("Tehtävä 3");
+            Console.WriteLine("Tehtävä 3: ");
             Console.WriteLine("Anna numero 0-9");
             string nro09 = Console.ReadLine();
             switch(nro09)
@@ -83,7 +83,64 @@ namespace ehtolauseet1
             }
 
             // Tehtävä 4: Ohjelma kysyy 5 kokonaislukua ja kertoo mikä on isoin.
-            //keskeN  
+            Console.WriteLine("Tehtävä 4: ");
+            Console.WriteLine("Sinulta kysytään viisi kokonaislukua yksi kerrallaan ja lopulta ohjelma kertoo mikä luku niistä oli isoin.");
+            Console.WriteLine("Syötä 1. kokonaisluku: ");
+            int koko1 = int.Parse(Console.ReadLine());
+            int suuurin = koko1;
+            Console.WriteLine("Syötä 2. kokonaisluku: ");
+            int koko2 = int.Parse(Console.ReadLine());
+            if (koko2 > suuurin) { suuurin = koko2; }
+            Console.WriteLine("Syötä 3. kokonaisluku: ");
+            int koko3 = int.Parse(Console.ReadLine());
+            if (koko3 > suuurin) { suuurin = koko3; }
+            Console.WriteLine("Syötä 4. kokonaisluku: ");
+            int koko4 = int.Parse(Console.ReadLine());
+            if (koko4 > suuurin) { suuurin = koko4; }
+            Console.WriteLine("Syötä 5. kokonaisluku: ");
+            int koko5 = int.Parse(Console.ReadLine());
+            if (koko5 > suuurin) { suuurin = koko5; }
+            Console.WriteLine("Suurin syötetty kokonaisluku on " + suuurin);
+
+
+            // Tehtävä 5: Ohjelma kysyy halutaanko syöttää kokonaisluku, desimaaliluku vai merkkijono.
+            // Jos käyttäjä syöttää luvun, kasvatetaan lukua yhdellä.
+            // Jos käyttäjä syöttää merkkijonon, lisätään loppuun '*'-merkki
+
+            Console.WriteLine("Tehtävä 5: ");
+            Console.WriteLine("Hei!");
+            Console.WriteLine("Haluatko syöttää 1:kokonaisluvun, 2:desimaaliluvun vai 3:merkkijonon?");
+            Console.WriteLine("Kerro numerolla(1-3): ");
+            string qq = Console.ReadLine();
+            zzz:
+            switch(qq)
+            {
+                case "1":
+                    Console.WriteLine("Syötä kokonaisluku: ");
+                    int num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Syötit luvun: " + num1 + ", Kasvatin lukuasi yhdellä, se on nyt: " + (num1 + 1));
+                    break;
+
+                case "2":
+                    Console.WriteLine("Syötä desimaaliluku: ");
+                    double num2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Syötit luvun: " + num2 + ", Kasvatin lukuasi yhdellä, se on nyt: " + (num2 + 1));
+                    break;
+
+                case "3":
+                    Console.WriteLine("Kirjoita jotain: ");
+                    string tecksti1 = Console.ReadLine();
+                    Console.WriteLine("Kirjoitit: '" + tecksti1 + "', lisäsin sen loppuun '*'-merkin, lopputulos: " + tecksti1 + "*");
+                    break;
+
+                default:
+                    Console.WriteLine("Virheellinen syöte, anna numero 1-3");
+                    qq = Console.ReadLine();
+                    goto zzz;
+
+            }
+
+
         }
     }
 }
